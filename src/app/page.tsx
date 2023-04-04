06cc336d129ef6dd6e7c609e1bd3b2c1d4cdaa3a
MuhammadMuzammilKhan951 <115939283+MuhammadMuzammilKhan951@users.noreply.github.com>
2023-04-04 15:38:29 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import head from './head'
 import Dropdown from './dropdown'
 export default function page() {
   return (
-    <div className=' h-[3000px] bg-white'>
+    <div className=' h-[9000px] bg-white'>
       <div className='sticky z-20 bg-transparent inset-0  backdrop-blur-md'>
         <div className=' flex justify-start pt-4 pl-4 '>
           <div>
